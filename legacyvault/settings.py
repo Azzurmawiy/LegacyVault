578cf1a38fc00a73ca5738227d7025a3055ad8a4
Azzurmawiy <awwalusman427@gmail.com>
2026-04-02 18:16:57 +0100

    fix: csrf

--- legacyvault/settings.py
+++ legacyvault/settings.py
@@ -113,7 +113,10 @@ Q_CLUSTER = {
     'label': 'Django Q',
     'orm': 'default'
 }
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['127.0.0.1', 'localhost'])
 
 CSRF_TRUSTED_ORIGINS = env.list('CSRF_TRUSTED_ORIGINS', default=[])
-CSRF_COOKIE_SECURE = True
-SESSION_COOKIE_SECURE = True
+
+CSRF_COOKIE_SECURE = not DEBUG
+SESSION_COOKIE_SECURE = not DEBUG
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
